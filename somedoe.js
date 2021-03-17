128b46bda8a78f767911e7c5c6109c167f33878f
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-03-17 07:38:21 -0300

    First commit

---
+++ somedoe.js
@@ -0,0 +1,10 @@
+var person = {
+  firstName: "john",
+  lastName : "doe",
+  id     : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+console.log(`Mi nombre es ${person.fullName()}`)
