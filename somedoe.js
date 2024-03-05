eae0b17bad3d8af61da3b1db6dbc096e078ea510
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2024-03-05 08:38:16 -0300

    sadas

--- somedoe.js
+++ somedoe.js
@@ -1,10 +1,7 @@
 var person = {
   name: "John",
   lastName : "Doe",
-  id     : 5566,
-  fullName : function() {
-    return this.name + " " + this.lastName;
-  }
+  id     : 5566
 };
 
 console.log(`Mi nombre es ${person.fullName()}`)
